fd2952d01ae7a567a1279e2e47d75da86f6c97a4
raushan <kumarraushan2615@gmail.com>
2024-12-22 13:20:23 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className='text-3xl bg-green-700'>building netflix gpt </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
